7637c0a261ab51265b9a900949d36278ea96833a
chris <chrisbite121@yahoo.co.uk>
2018-05-10 05:37:02 +0100

    complete

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Moment from "react-moment";
+import { deleteEducation } from "../../actions/profileActions";
+
+export class Education extends Component {
+  static propTypes = {
+    deleteEducation: PropTypes.func.isRequired
+  };
+
+  onDeleteClick(id) {
+    this.props.deleteEducation(id);
+  }
+
+  render() {
+    const education = this.props.education.map(edu => (
+      <tr key={edu._id}>
+        <td>{edu.school}</td>
+        <td>{edu.degree}</td>
+        <td>
+          <Moment format="DD/MM/YYYY">{edu.from}</Moment> - <span> </span>
+          {edu.to === null ? (
+            "Now"
+          ) : (
+            <Moment format="DD/MM/YYYY">{edu.to}</Moment>
+          )}
+        </td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={this.onDeleteClick.bind(this, edu._id)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+
+    return (
+      <div>
+        <h4 className="mb-4">Education</h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>School</th>
+              <th>Degree</th>
+              <th>Years</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>{education}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {
+  deleteEducation
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Education);
